ac1bd437521089df889c878c39b3717932ffda0a
smiller <smiller@bardavon.com>
2018-05-25 20:03:09 -0500

    fixed imports

--- kryptobot/listeners/base_listener.py
+++ kryptobot/listeners/base_listener.py
@@ -7,7 +7,7 @@ class BaseListener(Core):
 
     # publisher is a pubsub to subscribe to
     def __init__(self, publisher, publisher_params, config=None):
-        super().__init__(None, config)
+        super().__init__(config)
         publisher.subscribe(self.on_message, **publisher_params)
         self.publisher = publisher
 
