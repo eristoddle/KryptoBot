77bda771fd34e770f80f73565770719e59837309
eristoddle <stephanmil@gmail.com>
2018-06-30 23:44:33 -0500

    deactivate catalyst jobs - pandas error

--- kryptobot/workers/celery.py
+++ kryptobot/workers/celery.py
@@ -2,10 +2,11 @@ from .app import app
 
 app.conf.update(
     include=[
-       'kryptobot.workers.catalyst.tasks',
+       # catalyst pandas error
+       # 'kryptobot.workers.catalyst.tasks',
        'kryptobot.workers.harvester.tasks',
        'kryptobot.workers.strategy.tasks',
-       'kryptobot.workers.core.tasks',
+       # 'kryptobot.workers.core.tasks',
        'kryptobot.workers.t2.tasks',
        'kryptobot.workers.tasks'
        ],
